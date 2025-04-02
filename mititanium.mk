573983fcedc8bab85342377b313aac52d202981c
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-02 14:27:19 +0000

    mititanium-common: Remove deprecated Lineage Trust HAL
    * Now we set USB restriction through the USB HAL
    
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -384,10 +384,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
