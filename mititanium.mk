898f98f8c7fdcb258c515fa1b77d4f2bf7d8e6c9
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-02 10:08:36 +0000

    mititanium-common: Disable UFFD GC via OVERRIDE_ENABLE_UFFD_GC
    [RennAlt] Our kernel (4.19) does not support features needed for UFFD GC.
    
    Leaving it enabled results in long waiting time on the first boot.
    
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -27,6 +27,9 @@ OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG ?= xxhdpi
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
