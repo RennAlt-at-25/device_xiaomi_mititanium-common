9ff4a4fb068cd5e359f632ab937256bb28cbe5cc
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-02 14:25:33 +0000

    mititanium-common: Switch to USB 1.3-basic HAL
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -390,7 +390,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Tethering
 PRODUCT_PACKAGES += \
