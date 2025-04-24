da559a2735300b308ee0570f05cca1f0fc53c281
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-23 22:17:26 -0400

    mititanium-common: Explicitly set as an non-AB target
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -10,14 +10,17 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Kernel
+TARGET_KERNEL_VERSION ?= 4.9
+
+# OTA
+AB_OTA_UPDATER := false
+
 # Signed
 -include vendor/lineage-priv/keys/keys.mk
 
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
-
-# Kernel
-TARGET_KERNEL_VERSION ?= 4.9
  
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
