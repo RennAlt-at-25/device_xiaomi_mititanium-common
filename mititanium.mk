c9908bec44ca4073aac7aeffa50fbe3f7d4e64ac
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-20 15:16:57 +0000

    mititanium-common: set TARGET_KERNEL_VERSION
    by default it is built with kernel flags 4.9, because msm8953 only ysl and oxygen already have kernel 4.19
    
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -12,6 +12,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
+
+# Kernel
+TARGET_KERNEL_VERSION ?= 4.9
  
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
