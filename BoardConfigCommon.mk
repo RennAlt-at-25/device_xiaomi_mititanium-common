8e906d14b2c67e4c40e06476c3aa73290fd0263e
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-20 15:00:05 +0000

    mititanium-common: Bump target-level to 5 for kernel 4.19
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,11 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+ifeq ($(TARGET_KERNEL_VERSION),4.19)
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest_k4.19.xml
+else
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest_k4.9.xml
+endif
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 # Init
