28633eef4b5b6a7d734bf99d797f8dfae36bda8e
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-22 07:33:51 +0000

    mititanium-common: Inherit Mi-Thorium QCOM HALs
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -396,6 +396,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Mi thorium qcom-hals
+$(call inherit-product-if-exists, hardware/mithorium-4.9/mithorium_qcom_hals.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/mititanium-common/mititanium-common-vendor.mk)
 
