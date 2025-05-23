e3a62565a9c72658df73ab8eef345fc22248489f
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-23 10:03:29 +0000

    mititanium-common: Switch to QTI Memtrack AIDL HAL
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -152,11 +152,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
-    memtrack.msm8953 \
+    vendor.qti.hardware.memtrack-service \
     libqdMetaData.system \
     libtinyxml \
     libvulkan \
