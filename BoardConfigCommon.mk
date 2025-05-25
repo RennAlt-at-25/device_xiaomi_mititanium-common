0b9143269b702ccc5c3f39c901259c005445b217
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-05-25 21:29:59 +0000

    mititanium-common: Add selinux fix for Lineage Health HAL
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,6 +133,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+BOARD_SEPOLICY_M4DEFS += \
+    sysfs_battery_supply=vendor_sysfs_battery_supply \
+    sysfs_usb_supply=vendor_sysfs_usb_supply
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
