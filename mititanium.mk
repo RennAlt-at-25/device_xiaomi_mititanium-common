72856d57a7d55efef827ba989980392c3c119a6e
itzkanasama <164613153+itzkanasama@users.noreply.github.com>
2025-06-06 07:34:20 +0000

    Revert "mititanium-common: Move to new RFS install_symlink targets"
    This reverts commit 38ece4ff4c925f3be4990eea4699be1ea32eda3c.

--- mititanium.mk
+++ mititanium.mk
@@ -7,9 +7,6 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Add common definitions for Qualcomm
-$(call inherit-product, hardware/qcom-caf/common/common.mk)
-
 #
 # All components inherited here go to system image
 #
