11ef05df19ef6a00da803ab98b914561628f4513
itzkanasama <164613153+itzkanasama@users.noreply.github.com>
2025-06-06 05:12:16 -0400

    mititanium-common: add hardware/mititanium-module

--- mititanium.mk
+++ mititanium.mk
@@ -323,7 +323,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mititanium-module
 
 # Telephony
 PRODUCT_PACKAGES += \
